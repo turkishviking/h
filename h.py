4307cff929cb857854123b8dbb226b55a9acad1a
Charlie <c.lutaud@hotmail.fr>
2023-11-19 16:22:00 +0100

    H

--- h.py
+++ h.py
@@ -4,16 +4,7 @@ from h_lib import processImage
 import os, random, time
 from threading import Thread, Lock
 import sys
-
-############################################
-"""       PARAMETERS GO HERE <3          """
-############################################
-FOLDER = "/home/charlie/Pictures/h"
-SIZE = 800, 600
-BORDER_RADIUS = 150
-TIME_TO_CHANGE_GIF = 2 #secondes!
-FPS = 60.
-############################################
+from h_settings import *
 
 pygame.display.set_caption('Hachinator')
 scriptDir = os.path.dirname(os.path.abspath(sys.argv[0]))
---
+++ h_settings.py
@@ -0,0 +1,10 @@
+
+############################################
+"""       PARAMETERS GO HERE <3          """
+############################################
+FOLDER = "/home/charlie/Pictures/h"
+SIZE = 800, 600
+BORDER_RADIUS = 150
+TIME_TO_CHANGE_GIF = 2 #secondes!
+FPS = 60.
+############################################
